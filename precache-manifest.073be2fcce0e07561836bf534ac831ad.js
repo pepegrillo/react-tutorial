f84b5f93231fd30befc8d7dc0174e8c7903c2f06
pepegrillo <josuea3lopezgmail.com>
2018-11-22 21:20:01 +0100

    Updates

---
+++ precache-manifest.073be2fcce0e07561836bf534ac831ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63602fabe2f07e92b031",
+    "url": "/pepegrillo/react-tutorial.git/static/js/runtime~main.63602fab.js"
+  },
+  {
+    "revision": "e9cdf543b836959c288b",
+    "url": "/pepegrillo/react-tutorial.git/static/js/main.e9cdf543.chunk.js"
+  },
+  {
+    "revision": "65ce664db22ebc0891b6",
+    "url": "/pepegrillo/react-tutorial.git/static/js/1.65ce664d.chunk.js"
+  },
+  {
+    "revision": "e9cdf543b836959c288b",
+    "url": "/pepegrillo/react-tutorial.git/static/css/main.cc311e93.chunk.css"
+  },
+  {
+    "revision": "35a28616f1e850090ae656f69842bbd2",
+    "url": "/pepegrillo/react-tutorial.git/index.html"
+  }
+];
